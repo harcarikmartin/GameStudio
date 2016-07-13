236e98719ae23a06a2d6403a44bf3cc514ba45e8
harcarikmartin <harcarikmartin@icloud.com>
2016-07-13 15:56:24 +0200

    160713 16.00

--- src/sk/tsystems/gamestudio/service/RatingService.java
+++ src/sk/tsystems/gamestudio/service/RatingService.java
@@ -1,10 +1,7 @@
 package sk.tsystems.gamestudio.service;
 
-import java.util.List;
-import sk.tsystems.gamestudio.entity.Rating;
-
 public interface RatingService {
-	void add(Rating rating);
-	
-	List<Rating> findRatingForGame(String game);
+//	void add(Rating rating);
+//	
+//	List<Rating> findRatingForGame(String game);
 }
